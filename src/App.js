838bf957d5067235af52d8f4817ef90548d4152a
theraushan17 <theraushan17@gmail.com>
2022-02-18 09:36:26 +0530

    project

--- src/App.js
+++ src/App.js
@@ -1,25 +1,71 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState ,useEffect} from 'react';
+import SquareComponent from "./SquareComponent";
+
+const initialState = ["", "", "", "", "", "", "", "", "", ""];
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    const [gameState, updateGameState] = useState(initialState);
+    const [isXChance, updateIsXChance] = useState(false);
+
+    const onUserClicked = (index) => {
+        let strings = Array.from(gameState);
+        strings[index] = isXChance ? "X" : "0";
+        updateGameState(strings);
+        updateIsXChance(!isXChance);
+        
+    }
+
+    useEffect(() => {
+        let winner = checkWinner();
+        if (winner) {
+            alert(`Congratulation ${winner} won the Game !`)
+            updateGameState(initialState)
+
+        }
+    }, [gameState])
+
+    const checkWinner = () => {
+        const lines = [
+            [0, 1, 2],
+            [3, 4, 5],
+            [6, 7, 8],
+            [0, 3, 6],
+            [1, 4, 7],
+            [2, 5, 8],
+            [0, 4, 8],
+            [2, 4, 6],
+        ];
+
+        for (let i = 0; i < lines.length; i++) {
+            const [a, b, c] = lines[i];
+            if (gameState[a] && gameState[a] === gameState[b] && gameState[a] === gameState[c]) {
+                return gameState[a];
+            }
+        }
+        return null;
+    }
+
+    return (
+        <div className="app-header">
+            <p className="heading-text">Play Tic Tac Toe Game </p>
+            <div className=" row jc-center">
+                <SquareComponent className="b-bottom-right" state={gameState[0]} onClick={() => onUserClicked(0)} />
+                <SquareComponent className="b-bottom-right" state={gameState[1]} onClick={() =>  onUserClicked(1)} />
+                <SquareComponent className="b-bottom"  state={gameState[2]} onClick={() => onUserClicked(2)} />
+            </div>
+            <div className="row jc-center">
+                <SquareComponent className="b-bottom-right"  state={gameState[3]} onClick={() => onUserClicked(3)} />
+                <SquareComponent className="b-bottom-right" state={gameState[4]} onClick={() => onUserClicked(4)} />
+                <SquareComponent className="b-bottom" state={gameState[5]} onClick={() => onUserClicked(5)} />
+            </div>
+            <div className="row jc-center">
+                <SquareComponent className="b-right" state={gameState[6]} onClick={() => onUserClicked(6)} />
+                <SquareComponent className="b-right" state={gameState[7]} onClick={() => onUserClicked(7)} />
+                <SquareComponent state={gameState[8]} onClick={() => onUserClicked(8)} />
+            </div>
+            <button className="clear-button" onClick={ ()=> updateGameState(initialState) }>Clear Game</button>
+        </div>
+    );
 }
 
 export default App;
